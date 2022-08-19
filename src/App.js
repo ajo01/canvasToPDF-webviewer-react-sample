b46f7aa64ae48de3f7fd5f39cddfe77b99417bec
Amy Jo <amyjojjm2001@gmail.com>
2022-08-19 12:20:33 -0700

    canvasToPDF integration and rectangle annotation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
-import React, { useRef, useEffect } from 'react';
-import WebViewer from '@pdftron/webviewer';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import WebViewer from "@pdftron/webviewer";
+import "./App.css";
+import canvasToPDF from "@pdftron/canvas-to-pdf";
 
 const App = () => {
   const viewer = useRef(null);
 
-  // if using a class, equivalent of componentDidMount 
   useEffect(() => {
     WebViewer(
       {
-        path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        path: "/webviewer/lib",
+        initialDoc: "/files/PDFTRON_about.pdf",
       },
-      viewer.current,
-    ).then((instance) => {
+      viewer.current
+    ).then(async (instance) => {
       const { documentViewer, annotationManager, Annotations } = instance.Core;
 
-      documentViewer.addEventListener('documentLoaded', () => {
+      documentViewer.addEventListener("documentLoaded", async () => {
         const rectangleAnnot = new Annotations.RectangleAnnotation({
           PageNumber: 1,
           // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
+          X: 0,
+          Y: 0,
+          Width: 300,
+          Height: 300,
+          Author: annotationManager.getCurrentUser(),
         });
+
+        const draw = (ctx) => {
+          ctx.fillStyle = "red";
+          ctx.lineWidth = "20";
+          ctx.strokeStyle = "black";
+          ctx.rect(50, 50, 250, 250);
+          ctx.fill();
+          ctx.stroke();
+        };
+
+        const blob = await canvasToPDF(draw);
+        const doc = await instance.Core.createDocument(blob, {
+          extension: "pdf",
+        });
+
+        rectangleAnnot.addCustomAppearance(doc, { pageNumber: 1 });
 
         annotationManager.addAnnotation(rectangleAnnot);
         // need to draw the annotation otherwise it won't show up until the page is refreshed
